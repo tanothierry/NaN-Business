8cc15595ff3f84dc413dcddfb5ed770f5f126aa6
tanothierry <tano.tano@uvci.edu.ci>
2019-12-31 05:06:19 +0100

    ajout du formulaire

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,117 +1,18 @@
-const canvas = document.getElementById("canvas");
-            const ctx = canvas.getContext("2d");
+$(function(){
+    var navbar = $('.navbar');
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() <= 200){
+            navbar.removeClass('navbar-scroll');
+		} else {
+            navbar.addClass('navbar-scroll');
+		}
+	});
+});
 
-            let img = new Image();
-            let fileName = "";
+//Pour jquery:
+//remplacer querySelector par $
+//ajouter doc rdy
 
-            const downloadBtn = document.getElementById("download-btn");
-            const uploadFile = document.getElementById("upload-file");
-            const revertBtn = document.getElementById("revert-btn");
 
-            // Filter & Effect Handlers
-            document.addEventListener("click", e => {
-            if (e.target.classList.contains("filter-btn")) {
-                if (e.target.classList.contains("brightness-add")) {
-                Caman("#canvas", img, function() {
-                    this.brightness(5).render();
-                });
-                } else if (e.target.classList.contains("brightness-remove")) {
-                Caman("#canvas", img, function() {
-                    this.brightness(-5).render();
-                });
-                } else if (e.target.classList.contains("contrast-add")) {
-                Caman("#canvas", img, function() {
-                    this.contrast(5).render();
-                });
-                } else if (e.target.classList.contains("contrast-remove")) {
-                Caman("#canvas", img, function() {
-                    this.contrast(-5).render();
-                });
-                } else if (e.target.classList.contains("saturation-add")) {
-                Caman("#canvas", img, function() {
-                    this.saturation(5).render();
-                });
-                } else if (e.target.classList.contains("saturation-remove")) {
-                Caman("#canvas", img, function() {
-                    this.saturation(-5).render();
-                });
-                } else if (e.target.classList.contains("vibrance-add")) {
-                Caman("#canvas", img, function() {
-                    this.vibrance(5).render();
-                });
-                } else if (e.target.classList.contains("vibrance-remove")) {
-                Caman("#canvas", img, function() {
-                    this.vibrance(-5).render();
-                });
-                } else if (e.target.classList.contains("vintage-add")) {
-                Caman("#canvas", img, function() {
-                    this.vintage().render();
-                });
-                } else if (e.target.classList.contains("lomo-add")) {
-                Caman("#canvas", img, function() {
-                    this.lomo().render();
-                });
-                } else if (e.target.classList.contains("clarity-add")) {
-                Caman("#canvas", img, function() {
-                    this.clarity().render();
-                });
-                } else if (e.target.classList.contains("sincity-add")) {
-                Caman("#canvas", img, function() {
-                    this.sinCity().render();
-                });
-                } else if (e.target.classList.contains("crossprocess-add")) {
-                Caman("#canvas", img, function() {
-                    this.crossProcess().render();
-                });
-                } else if (e.target.classList.contains("pinhole-add")) {
-                Caman("#canvas", img, function() {
-                    this.pinhole().render();
-                });
-                } else if (e.target.classList.contains("nostalgia-add")) {
-                Caman("#canvas", img, function() {
-                    this.nostalgia().render();
-                });
-                } else if (e.target.classList.contains("hermajesty-add")) {
-                Caman("#canvas", img, function() {
-                    this.herMajesty().render();
-                });
-                }
-            }
-            });
 
-            
-
-            // Upload File
-            uploadFile.addEventListener("change", () => {
-            // Get File
-            const file = document.getElementById("upload-file").files[0];
-            // Init FileReader API
-            const reader = new FileReader();
-
-            // Check for file
-            if (file) {
-                // Set file name
-                fileName = file.name;
-                // Read data as URL
-                reader.readAsDataURL(file);
-            }
-
-            // Add image to canvas
-            reader.addEventListener(
-                "load",
-                () => {
-                // Create image
-                img = new Image();
-                // Set image src
-                img.src = reader.result;
-                // On image load add to canvas
-                img.onload = function() {
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    ctx.drawImage(img, 0, 0, img.width, img.height);
-                    canvas.removeAttribute("data-caman-id");
-                };
-                },
-                false
-            );
-            });
